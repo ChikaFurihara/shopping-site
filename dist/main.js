788bc79098e9feba3a22412b5e2d21f91901e55b
Chica Furihara <chica.vivi0321@gmail.com>
2017-10-03 08:16:15 +0000

    top_sp_ver.update

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: Boolean,
+            items: 1,
+            autoplay: true,
+        }
+    );
+});
