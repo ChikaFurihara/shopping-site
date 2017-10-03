f840f78ef8a0fb9043f421fc89ff2a11fca3a13d
Chica Furihara <chica.vivi0321@gmail.com>
2017-10-03 09:26:23 +0000

    2nd

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,40 @@
+//　カルーセル
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel(
         {
             loop: true,
             nav: false,
-            dots: Boolean,
+            dots: true,
             items: 1,
             autoplay: true,
         }
     );
 });
+
+//　ハンバーガーメニュー
+$(document).ready(function(){
+  $("#menu_js").click(function(){
+    $(".sp_navi_li").slideToggle();
+  });
+});
+
+
+//　スクロール
+$(function(){
+    $('a[href^=#]').click(function(){
+        var speed = 1000;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
+
+// $('.cr_btn').click(function(){
+//   var id = $(this).attr('href');
+//   var position = $(id).offset().top;
+//   $('html,body').animate({
+//     'scrollTop':position
+//   },1000);
+// });
