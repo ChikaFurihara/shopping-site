f7de332a5c21a11649c2d6ec919b35abfea5619d
Chica Furihara <chica.vivi0321@gmail.com>
2017-11-02 03:30:24 +0000

    fix

--- dist/cotegory/main1.js
+++ dist/cotegory/main1.js
@@ -13,3 +13,19 @@ $(document).ready(function(){
     }
   );
 });
+
+//　ハンバーガーメニュー
+$(document).ready(function(){
+  $("#menu_js").click(function(){
+    $(".sp_navi_li").slideToggle();
+  });
+});
+
+
+//　category内スライドショー
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
